e3356e792cb2607ce616cf974e4f83f8ffd64937
Sraddha--aa <sraddha@mozilor.com>
2023-08-18 22:25:16 +0530

    upd

---
+++ Desktop/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Simple React Website</h1>
+      </header>
+      <main className="App-main">
+        <p>Welcome to our simple React website!</p>
+        <p>This is some content on our website.</p>
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2023 Simple React Website</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
